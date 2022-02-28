16c4544eb65e735c264854d9ebd1e999fc0338d2
Mateo GB <gbteo2003@gmail.com>
2022-02-28 18:12:55 -0500

    se crea controlador de llenado

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -3,8 +3,15 @@
 let productos=[
 
 
-   {nombre:"figura goku",precio:25000,descripcion:"figura a escala"},
-   {nombre:"camiseta goku",precio:50000,descripcion:"camiseta Original"}
+   {nombre:"figura goku",precio:25000,descripcion:"figura de acción goku niño ssj3"},
+   {nombre:"camiseta goku",precio:50000,descripcion:"camiseta original goku ultrainstinto"},
+   {nombre:"esferas del dragon",precio:100000,descripcion:"kit completo esferas del dragon"},
+   {nombre:"gorra goku blue",precio:15000,descripcion:"toei animation gorra goku ssj blue"},
+   {nombre:"figura shenron",precio:30000,descripcion:"figura crystal shenlong con esferas"},
+   {nombre:"chaqueta dragon ball",precio:150000,descripcion:"chaqueta original dragon ball"},
+   {nombre:"mochila dragon ball",precio:60000,descripcion:"mochila dragon ball adaptable"},
+   {nombre:"funda goku ssj dios",precio:30000,descripcion:"funda silicon suave iphone X o XS"},
+   {nombre:"monopoly edición dbz",precio:70000,descripcion:"juego de mesa monopoly edición dbz"},
+   {nombre:"PS4 FAT skin dbz",precio:2500000,descripcion:"PS4 FAT 500gb personalizada con 2 mandos"}
 ]
-
 console.log(productos)
