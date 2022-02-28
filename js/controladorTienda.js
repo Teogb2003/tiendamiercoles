fc16f53d02771f425cb871d45e0bf0faacef1cca
Mateo GB <gbteo2003@gmail.com>
2022-02-28 11:09:53 -0500

    se crea el controlador de llenado

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,7 +1,7 @@
 //Controlando etiquetas
 
 //1. crear una variable para guardar una etiqueta
-let etiquetaTitulo = document.getElementById("titulo")
+/*let etiquetaTitulo = document.getElementById("titulo")
 console.log(etiquetaTitulo)
 
 
@@ -18,4 +18,25 @@ etiquetaFoto.src="img/logo.png"
 
 //Cambiar el estilo de la etiqueta
 etiquetaTitulo.classList.add("text-center")
-etiquetaTitulo.classList.add("text-danger")
+etiquetaTitulo.classList.add("text-danger")*/
+
+//TIENDA
+let productosNombre=["figura goku","camiseta goku"]
+let productosPrecio=[25000,50000]
+let productosPromocion=[true,false]
+
+console.log(productosNombre)
+console.log(productosNombre[1])
+
+//Creando objetos con JS
+let usuario={
+   
+    nombre:"Son Goku",
+    edad:42,
+    amigos:["krilin", "picoro", "vegeta"]
+
+}
+
+console.log(usuario)
+console.log(usuario.nombre)
+console.log(usuario.amigos[1])
---
+++ js/llenadoTienda.js
@@ -0,0 +1,10 @@
+//creando un arreglo de objetos
+
+let productos=[
+
+
+   {nombre:"figura goku",precio:25000,descripcion:"figura a escala"},
+   {nombre:"camiseta goku",precio:50000,descripcion:"camiseta Original"}
+]
+
+console.log(productos)
